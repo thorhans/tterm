050bdbc4aa024d6ab6a196b54cb92e6511cad5c7
ThH <thorbjoern.hansen@gmx.de>
2017-12-25 11:34:31 +0100

    New class `PKeyButton´.

---
+++ term/src/main/java/de/t2h/tterm/key/PKeyButton.java
@@ -0,0 +1,14 @@
+package de.t2h.tterm.key;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ */
+public class PKeyButton extends Button {
+  public PKeyButton(Context context) { super(context); }
+  public PKeyButton(Context context, AttributeSet attrs) { super(context, attrs); }
+  public PKeyButton(Context context, AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); }
+  public PKeyButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) { super(context, attrs, defStyleAttr, defStyleRes); }
+}
