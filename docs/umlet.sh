0295f07ec23b4f2b6bf706d2889c8ae896333a6c
ThH <thorbjoern.hansen@gmx.de>
2018-01-05 12:38:14 +0100

    Added shell script to start UMLet. Only works on *my* computer.

---
+++ docs/umlet.sh
@@ -0,0 +1,16 @@
+# View `UML.uxf´ in UMLet.
+# 
+# - TODO This works when I double-click it, but not when I start it from
+#   Bash???
+
+echo "'umlet.sh' can't start from shell."
+echo "Double click it in the Windows Explorer".
+echo
+
+# My current version of UMLet does not start with Oracle's Java 9, so
+# I use my Android Studio's JRE 8.
+# 
+/d/prg/android/studio/jre/bin/java   \
+    -Dsun.java2d.xrender=f           \
+    -jar c:/prg/java/umlet/umlet.jar \
+    -filename="UML.uxf"
